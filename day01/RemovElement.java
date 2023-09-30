bb154836632ca35bf3e749f004fe9badacd6e973
u7516709 <u7516709@anu.edu.au>
2023-09-30 23:59:01 +1000

    day01

---
+++ day01/RemovElement.java
@@ -0,0 +1,62 @@
+package day01;
+
+public class RemovElement {
+    /**
+     * 暴力解法
+     * @param nums 一个数组
+     * @param val 一个值
+     * @return 移除后数组的新长度
+     */
+    public int removeElement1(int[] nums, int val) {
+        int len=nums.length;
+        for (int i = 0; i < len; i++) {
+            if(nums[i]==val){  // 发现需要移除的元素，就将数组集体向前移动一位
+                for (int j = i+1; j < len; j++) {
+                    nums[j-1]=nums[j];
+                }
+                i--;//因为下标i以后的数值都向前移动了一位，所以i也向前移动一位
+                len--;// 此时数组的大小-1
+            }
+        }
+        return len;
+    }
+
+    /**
+     *快慢指针
+     * @param nums 一个数组
+     * @param val 一个值
+     * @return 移除后数组的新长度
+     */
+    public int removeElement2(int[] nums, int val) {
+        int slowIndex=0;
+        for (int fastIndex=0; fastIndex <nums.length ; fastIndex++) {
+            if(fastIndex!=val){
+                nums[slowIndex]=nums[fastIndex];
+                slowIndex++;
+            }
+        }
+        return slowIndex;
+    }
+
+    /**
+     *相向双指针
+     * @param nums 一个数组
+     * @param val 一个值
+     * @return 移除后数组的新长度
+     */
+    public int removeElement3(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(right >= 0 && nums[right] == val) right--;
+        while(left <= right) {
+            if(nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            }
+            left++;
+            while(right >= 0 && nums[right] == val) right--;
+        }
+        return left;
+    }
+
+}
