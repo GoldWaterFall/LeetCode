93948452347e3598afedc783ba4c15b87c4a9c98
u7516709 <u7516709@anu.edu.au>
2023-10-06 00:09:46 +1100

    day06

---
+++ day06/TwoSum.java
@@ -0,0 +1,25 @@
+package day06;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int temp = target - nums[i];   // 遍历当前元素，并在map中寻找是否有匹配的key
+            if(map.containsKey(temp)){
+                res[1] = i;
+                res[0] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);    // 如果没找到匹配对，就把访问过的元素和下标加入到map中
+        }
+        return res;
+    }
+
+}
