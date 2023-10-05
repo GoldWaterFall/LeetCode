8d14c01f1a64b1af85c7b02cf2bd5adde0ac1935
u7516709 <u7516709@anu.edu.au>
2023-10-05 23:49:57 +1100

    day06

---
+++ day06/IntersectionOfTwoArrays.java
@@ -0,0 +1,37 @@
+package day06;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+    /**
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+        //遍历数组1
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        //遍历数组2的过程中判断哈希表中是否存在该元素
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                resSet.add(i);
+            }
+        }
+        int[] arr = new int[resSet.size()];
+        int j = 0;
+        for(int i : resSet){
+            arr[j++] = i;
+        }
+
+        return arr;
+    }
+}
