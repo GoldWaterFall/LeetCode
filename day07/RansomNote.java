9dbc97e3382a1d916f26ca7a9d26293aca69d863
u7516709 <u7516709@anu.edu.au>
2023-10-07 00:13:40 +1100

    day06

---
+++ day07/RansomNote.java
@@ -0,0 +1,37 @@
+package day07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+    /**
+     * 找字母的题，尤其找字母数量，直接用数组做哈希表法
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] record = new int[26];
+
+        for(char c : magazine.toCharArray()){
+            record[c - 'a'] += 1;
+        }
+
+        for(char c : ransomNote.toCharArray()){
+            record[c - 'a'] -= 1;
+        }
+
+        // 如果数组中存在负数，说明ransomNote字符串总存在magazine中没有的字符
+        for(int i : record){
+            if(i < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
