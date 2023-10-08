7ad3f696cf6d16b649ff6c771e991f544c44500d
u7516709 <2038547278@qq.com>
2023-10-08 23:57:26 +1100

    day09

---
+++ day09/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,38 @@
+package day09;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    /**
+     * 前缀表实现
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i))
+                j = next[j - 1];
+            if (needle.charAt(j) == haystack.charAt(i))
+                j++;
+            if (j == needle.length())
+                return i - needle.length() + 1;
+        }
+        return -1;
+    }
+    private void getNext(int[] next, String s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i))
+                j = next[j - 1];
+            if (s.charAt(j) == s.charAt(i))
+                j++;
+            next[i] = j;
+        }
+    }
+
+}
