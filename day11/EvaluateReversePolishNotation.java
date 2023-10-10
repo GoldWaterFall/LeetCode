bb084b452bb20cce71a8846fa80b3922400f8649
u7516709 <2038547278@qq.com>
2023-10-11 00:33:40 +1100

    day11

---
+++ day11/EvaluateReversePolishNotation.java
@@ -0,0 +1,26 @@
+package day11;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList();
+        for (String s : tokens) {
+            if ("+".equals(s)) {        // leetcode 内置jdk的问题，不能使用==判断字符串是否相等
+                stack.push(stack.pop() + stack.pop());      // 注意 - 和/ 需要特殊处理
+            } else if ("-".equals(s)) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if ("*".equals(s)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(s)) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2 / temp1);
+            } else {
+                stack.push(Integer.valueOf(s));
+            }
+        }
+        return stack.pop();
+    }
+}
